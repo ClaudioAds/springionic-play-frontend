50453fa80f23c04c7bff9e7342db7ab07d51cb76
ClaudioAds <claudio.blz25@gmail.com>
2021-11-27 21:03:30 -0300

    creating shopping cart page

---
+++ src/services/domain/cart.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { Cart } from "../../models/cart";
+import { ProdutoDTO } from "../../models/produto.dto";
+import { StorageService } from "../storage.service";
+
+
+@Injectable()
+export class CartService {
+
+    constructor(public storage: StorageService) {
+    }
+
+    createOrClearCart(): Cart {
+        let cart: Cart = { items: [] };
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    getCart(): Cart {
+        let cart: Cart = this.storage.getCart();
+        if (cart == null) {
+            cart = this.createOrClearCart();
+        }
+        return cart;
+    }
+
+    addProduto(produto: ProdutoDTO): Cart {
+        let cart = this.getCart();//pegando o carrinho
+        //verificando dentro da lista se tem algum produto com mesmo item
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position == -1) {
+            cart.items.push({quantidade: 1, produto: produto});
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+}
