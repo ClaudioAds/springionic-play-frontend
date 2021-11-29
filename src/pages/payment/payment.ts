24cb01c1e0ea8d0fbbfad8174ac3f57197106162
ClaudioAds <claudio.blz25@gmail.com>
2021-11-29 00:51:38 -0300

    payment choice screen

---
+++ src/pages/payment/payment.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDto } from '../../models/pedido.dto';
+
+@IonicPage()
+@Component({
+  selector: 'page-payment',
+  templateUrl: 'payment.html',
+})
+export class PaymentPage {
+
+  pedido: PedidoDto;
+
+  parcelas: number[] = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10];
+
+  //para controlar os dados das entradas na tabela 
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+      //vai receber o objeto que vem na navegação da página d endereço
+      this.pedido = this.navParams.get('pedido');
+
+    this.formGroup = this.formBuilder.group({
+      numeroDeParcelas: [1, Validators.required],
+      "@type": ["pagamentoComCartao", Validators.required]
+    });
+  }
+
+  nextPage(){
+    this.pedido.pagamento = this.formGroup.value;
+    console.log(this.pedido);
+  }
+
+}
--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -54,9 +54,10 @@ export class PickAddressPage {
     }
   }
 
+  //método para a escolha do endereço
   nextPage(item: EnderecoDTO) {
     this.pedido.enderecoDeEntrega = {id: item.id};
-    console.log(this.pedido);
+    this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
 }
